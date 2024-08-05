20136c9d9713bdd6c6c210e0a94f8da771b0cc04
DotVR46 <maggot8950@gmail.com>
2024-08-05 19:23:56 +0600

    Добавлено приложение новостей

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from filebrowser.sites import site
+
 
 urlpatterns = [
-    path('admin/filebrowser/', site.urls),
-    path('grappelli/', include("grappelli.urls")),
     path("admin/", admin.site.urls),
+    path("news/", include("news.urls")),
 ]
