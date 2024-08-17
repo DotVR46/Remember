3420189d9731d7b1d1d31a4350971f0d47ea5580
DotVR46 <maggot8950@gmail.com>
2024-08-17 19:44:58 +0600

    Модель Comment добавлена для отображения в админке

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from news.models import Article
+from news.models import Article, Comment
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "created", "published")
     prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "article", "created")
