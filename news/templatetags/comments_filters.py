7d71998a0fe39b5e0b0aeec73737f9441b9a0a7f
DotVr46 <maggot8950@gmail.com>
2024-09-30 00:09:08 +0600

    Переезд с venv на poetry

--- news/templatetags/comments_filters.py
+++ news/templatetags/comments_filters.py
@@ -5,6 +5,8 @@ register = template.Library()
 
 @register.filter
 def comment_declension(count):
+    # Выводим нужное слово в зависимости от количества комментариев
+
     if count % 10 == 1 and count % 100 != 11:
         return f"{count} комментарий"
     elif 2 <= count % 10 <= 4 and not (12 <= count % 100 <= 14):
