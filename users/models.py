669489227c25b35bd39af9b96853c36f3db0af0d
DotVr46 <maggot8950@gmail.com>
2024-10-18 00:12:27 +0600

    Модель профиля пользователя

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """Профиль пользователя"""
+
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name="Пользователь"
+    )
+    bio = models.TextField(max_length=500, blank=True, verbose_name="Краткая биография")
+    twitter = models.URLField(max_length=200, blank=True, verbose_name="Twitter")
+    facebook = models.URLField(max_length=200, blank=True, verbose_name="Facebook")
+    instagram = models.URLField(max_length=200, blank=True, verbose_name="Instagram")
+
+    def __str__(self):
+        return self.user.username
