8cf8a4d9fe6230a563543315386ee9801397648b
DotVr46 <maggot8950@gmail.com>
2024-11-15 22:15:08 +0600

    Страница профиля пользователя

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from users import views
 
 urlpatterns = [
     path("users/register", views.UserRegister.as_view(), name="register"),
+    path("users/profile/<int:pk>", views.ProfileView.as_view(), name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import generic
 
+from news.models import Article
 from users.forms import UserRegistrationForm
+from django.contrib.auth.models import User
 
 
 class UserRegister(generic.CreateView):
@@ -18,3 +21,27 @@ class UserRegister(generic.CreateView):
                 request, "registration/register_done.html", {"user": new_user}
             )
         return render(request, "registration/register.html", {"form": user_form})
+
+
+class ProfileView(generic.DetailView):
+    model = User
+    template_name = "pages/profile.html"
+    context_object_name = "profile"
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Получаем профиль пользователя
+        user = self.get_object()
+        # Получаем все статьи, написанные этим пользователем
+        user_articles = Article.objects.filter(author=user).order_by("-created_at")
+        # Настраиваем пагинацию
+        paginator = Paginator(user_articles, self.paginate_by)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        # Добавляем данные пагинации в контекст
+        context["page_obj"] = page_obj
+        context["articles"] = page_obj.object_list
+
+        return context
