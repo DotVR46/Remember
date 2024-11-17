eb0f2f75efe3a3a11a96747a755d1fc0be246f3e
DotVr46 <maggot8950@gmail.com>
2024-11-17 20:17:29 +0600

    Исправление ошибок

--- core/settings.py
+++ core/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["qc4apk-217-29-24-132.ru.tuna.am", "127.0.0.1", "localhost"]
+CSRF_TRUSTED_ORIGINS = ["https://qc4apk-217-29-24-132.ru.tuna.am"]
 
 # Application definition
 
